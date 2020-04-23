216d69a8ed0afef611da9ecfe955b61048c3263d
JonM0 <61517667+JonM0@users.noreply.github.com>
2020-04-24 00:42:27 +0200

    Add project files.

---
+++ PatchOperationOffsetField.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Xml;
+
+
+namespace Verse
+{
+    internal class PatchOperationOffsetField : PatchOperationPathed
+    {
+        private readonly double offset;
+
+        protected override bool ApplyWorker ( XmlDocument xml )
+        {
+            bool result = false;
+
+            foreach ( XmlNode parentNode in from object x in xml.SelectNodes( xpath )
+                                            select (XmlNode)x )
+            {
+                result = true;
+
+                double oldValue = double.Parse( parentNode.InnerText );
+
+                parentNode.InnerText = string.Format( "{0}", oldValue + offset );
+            }
+
+            return result;
+        }
+    }
+}
